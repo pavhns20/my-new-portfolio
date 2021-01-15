fb52f1d8a82de31e9bed2a67abe98c600911c7c2
pavhns20 <67502480+pavhns20@users.noreply.github.com>
2021-01-16 09:49:30 +1100

    added a back to top button

--- js/main.js
+++ js/main.js
@@ -107,3 +107,22 @@ tl.to("#line, #upper, #lower", { duration: 1, opacity: 0, ease: "none" }, "+=2")
 // TweenLite.set(contentToScroll, {
 //   y: -window.pageYOffset
 // });
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
